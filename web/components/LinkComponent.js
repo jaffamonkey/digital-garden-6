4b5fe616bffbf043a89300060d904d0b2ad91e58
Paul Littlebury <paullittlebury@gmail.com>
2022-04-18 22:18:27 +0100

    Delete web directory

--- web/components/LinkComponent.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function LinkComponent({ children, href }) {
-	return (
-		<Link href={href}>
-			<a>{children}</a>
-		</Link>
-	);
-}
--- web/lib/date.js
+++
@@ -1,5 +0,0 @@
-import { format, parseISO } from "date-fns";
-
-export function convertDate(date, displayFormat) {
-	return format(new Date(parseISO(date)), displayFormat);
-}
